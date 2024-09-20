903325155d794e2b44947c6c771878f6ebfd2232
Devi Sri Sai Charan <devisrisaicharan@gmail.com>
2024-09-20 09:29:06 +0530

    added company name in loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,9 @@
 import Loader_Video from "../assets/Loader_Video.mp4";
+import Unipool_Logo from "../assets/Unipool_Logo.png";
 
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen flex-col">
       <video
         src={Loader_Video}
         autoPlay
@@ -10,6 +11,15 @@ export default function Loader() {
         muted
         className="w-36"
       ></video>
+      <div className="flex absolute bottom-24 items-center">
+        <div>
+          <img src={Unipool_Logo} className="w-16" />
+        </div>
+        <div>
+          <h1 className="font-bold text-3xl">Uni<span className="text-yellow-500">pool</span></h1>
+          <p className="text-sm text-gray-400">Ride Together, Share Together</p>
+        </div>
+      </div>
     </div>
   );
 }
