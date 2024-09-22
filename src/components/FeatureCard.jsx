d45f45352775dfaa21c641fae74187ac611d84a6
Devi Sri Sai Charan <devisrisaicharan@gmail.com>
2024-09-22 12:21:44 +0530

    change hover color in feature section

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -7,7 +7,7 @@ export default function FeatureCard({title, text, setIndex, idx, index}){
     }
 
     return (
-        <div className={`hover:bg-gray-50 p-7 rounded-xl h-40 flex flex-col justify-center ${(index === idx) ? 'bg-gray-100' : ''} cursor-pointer`} onClick={changeIndex}>
+        <div className={`hover:bg-yellow-50 p-7 rounded-xl h-40 flex flex-col justify-center ${(index === idx) ? 'bg-yellow-200' : ''} cursor-pointer`} onClick={changeIndex}>
             <h3 className="text-[1.6vw] font-medium">{title}</h3>
             <p className="text-[1.2vw] text-gray-600">{text}</p>
         </div>
